38dfee06f73de84171aa5e4666c84481170b155b
Azadmehr Nohegar <nazadmehr888@gmail.com>
2023-10-20 19:43:12 +0330

    fix: added login redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import { lazy } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 const AuthPage = lazy(() => import("./auth"));
 
 function BasePage() {
   return (
     <Routes>
-      <Route index element={<>dashjboard</>} />
+      <Route index element={<Navigate to="auth" />} />
       <Route path="auth/*" element={<AuthPage />} />
     </Routes>
   );
